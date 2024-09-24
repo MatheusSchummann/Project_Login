5e5b887c04923c7232b4ce423ed89d72d24bd5d4
Matheus Schumann <matheusdschumann@gmail.com>
2024-09-24 16:03:37 -0300

    Iniciando rotas

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import{createBrowserRouter, RouterProvider} from "react-router-dom";
+
+import Home from "./Componets/pag_home/home.jsx";
+import Login_sign from './Componets/pag_login/pag_login/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/",
+    element: <Login_sign />,
+  },
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
